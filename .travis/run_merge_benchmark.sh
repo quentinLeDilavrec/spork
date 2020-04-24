307e1891be14ab153a693486789194f9f5c556b2
Simon Larsén <slarse@kth.se>
2020-04-24 21:42:18 +0200

    Add travis config

---
+++ .travis/run_merge_benchmark.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+echo "Compiling spork"
+mvn clean compile package -DskipTests
+spork_jar_path="$PWD/$(ls target/spork*.jar)"
+
+echo "Creating spork executable"
+echo "#! /bin/bash" > spork
+echo "java -jar $spork_jar_path" '$@' >> spork
+chmod 700 spork
+
+mv spork ~/
+
+cat "$TRAVIS_BUILD_DIR/.travis/gitconfig" >> ~/.gitconfig
+cat "$TRAVIS_BUILD_DIR/.travis/gitattributes" >> ~/.gitattributes
+
+git checkout benchmark
+
+cd scripts || exit
+
+python3 -m pip install -r requirements.txt
+python3 -m benchmark.cli run-git-merges \
+  -r spoon \
+  -g inria \
+  --merge-commits buildable_spoon_merges.txt \
+  --build \
+  --output spoon_git_merges_improved.csv
